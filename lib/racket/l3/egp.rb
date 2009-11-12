ea52773a12ec21d385fe366bae7c66f8d27069a3
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-12 23:47:52 +0000

    initial commit of egp
    git-svn-id: http://spoofed.org/racket/svn/trunk@43 64fbf49a-8b99-41c6-b593-eb2128c2192d

---
+++ lib/racket/l3/egp.rb
@@ -0,0 +1,43 @@
+# $Id$
+#
+# Copyright (c) 2008, Jon Hart 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY Jon Hart ``AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL Jon Hart BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Exterior Gateway protocol (EGP)
+# http://tools.ietf.org/html/rfc904
+module Racket
+class EGP < RacketPart
+  unsigned :version, 8
+  unsigned :type, 8
+  unsigned :code, 8
+  unsigned :status, 8
+  unsigned :checksum, 16
+  unsigned :asn, 16
+  unsigned :sequence, 16
+  # Payload
+  rest :payload
+end
+end
+# vim: set ts=2 et sw=2:
