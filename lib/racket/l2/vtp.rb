b0840879bc849f9a70df874641ac3fb88263a8d6
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-16 07:49:33 +0000

    Break out VTP messages individually
    git-svn-id: http://spoofed.org/racket/svn/trunk@70 64fbf49a-8b99-41c6-b593-eb2128c2192d

--- lib/racket/l2/vtp.rb
+++ lib/racket/l2/vtp.rb
@@ -28,7 +28,9 @@
 module Racket
 # VLAN Trunking Protocol (VTP)
 # http://en.wikipedia.org/wiki/VLAN_Trunking_Protocol
-class VTP < RacketPart
+# http://www.cisco.com/en/US/tech/tk389/tk689/technologies_tech_note09186a0080094c52.shtml
+# This is just a base class from which all VTP messages inherit and should never be used directly
+class VTPGeneric < RacketPart
   # VTP version (1-3)
   unsigned :version, 8 
   # Message code (summary advertisement, subset advertisement, advertisement request, VTP join)
@@ -39,14 +41,82 @@ class VTP < RacketPart
   unsigned :domain_length, 8
   # management domain name, zero padded to 32 bytes
   text :domain, 256
-  rest :payload
 
   # Adjust +domain_length+ and +domain+ accordingly prior to sending
   def fix!
     self.domain_length = self.domain.length 
     self.domain = self.domain.ljust(32, "\x00")
   end
+end
+
+# A raw VTP message
+class VTPRaw < VTPGeneric
+  rest :payload
+end
+
+class VTPSubsetAdvertisement < VTPGeneric
+  # Configuration revision
+  unsigned :revision, 32
+  # all of the vlan info fields
+  rest :payload
+
+  def add_vlan_info(status, type, id, mtu, index, name)
+    name_length = name.length
+    # zero pad name to a multiple of 4 bytes
+    name = name.length % 4 == 0 ? name : name.ljust(name.length + (4 - (name.length % 4)), "\x00")
+    length = 12 + name.length
+    @vlan_info << [length, status, type, name_length, id, mtu, index, name]
+  end
+
+  def fix!
+    @vlan_info.each do |v|
+      self.payload += v.pack("CCCCnnNa*")
+    end
+    super
+  end
+
+  def initialize(*args)
+    @vlan_info = []
+    super(*args)
+    self.code = 2 
+  end
+end
+
+class VTPSummaryAdvertisement < VTPGeneric
+  # Configuration revision number
+  unsigned :revision, 32
+  # Updater identity (IP)
+  octets :updater, 32
+  # update timestamp
+  unsigned :timestamp, 96
+  # MD5 digest of VTP password
+  text :md5, 128 
+  rest :payload
+
+  def initialize(*args)
+    super(*args)
+    self.code = 1
+  end
+end
+
+class VTPAdvertisementRequest < VTPGeneric
+  # This is used in cases in which there are several subset advertisements. If the first (n) subset advertisement has been received and the subsequent one (n+1) has not been received, the Catalyst only requests advertisements from the (n+1)th one.
+  unsigned :start, 32
+  rest :payload
+  def initialize(*args)
+    super(*args)
+    self.code = 3
+  end
 
 end
+
+
+class VTPJoin < VTPGeneric
+  def initialize(*args)
+    super(*args)
+    self.code = 4
+  end
+end
+
 end
 # vim: set ts=2 et sw=2:
