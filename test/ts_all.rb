39769265d3e2b4d84d9a4aa0b1b23ce8328b880b
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-23 01:27:41 +0000

    Move all tests in to their appropriate new directories based on layer
    git-svn-id: http://spoofed.org/racket/svn/trunk@81 64fbf49a-8b99-41c6-b593-eb2128c2192d

--- test/ts_all.rb
+++ test/ts_all.rb
@@ -2,5 +2,13 @@
 
 require 'rubygems'
 
-Dir.glob(File.join(File.dirname(__FILE__), '*.rb')).each { |f| require f }
+(2..5).each do |l|
+  Dir.glob(File.join(File.dirname(__FILE__) + "/l#{l}", '*.rb')).each do |f|
+    require f
+  end
+end
+
+Dir.glob(File.join(File.dirname(__FILE__) + "/misc", '*.rb')).each do |f|
+  require f
+end
 # vim: set ts=2 et sw=2:
