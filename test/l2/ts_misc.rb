435da63f71655834bcd89d2fa37ff9ba69eb256a
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-27 20:17:11 +0000

    Random mac
    git-svn-id: http://spoofed.org/racket/svn/trunk@104 64fbf49a-8b99-41c6-b593-eb2128c2192d

--- test/l2/ts_misc.rb
+++ test/l2/ts_misc.rb
@@ -7,7 +7,7 @@ require 'racket'
 
 class TestL2Misc <  Test::Unit::TestCase
   def test_convert
-    mac = "de:ad:ba:dc:af:e0"
+    mac = Racket::L2::Misc.long2mac(rand(2**48))
     long = Racket::L2::Misc.mac2long(mac)
     assert_equal(mac, Racket::L2::Misc.long2mac(long))
     assert_equal(long, Racket::L2::Misc.mac2long(mac))
