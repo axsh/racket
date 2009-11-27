358a24c1daaed122dfb3cb1b2bf629d3ce8e75a3
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-27 20:05:18 +0000

    Misc L2 test routines
    git-svn-id: http://spoofed.org/racket/svn/trunk@103 64fbf49a-8b99-41c6-b593-eb2128c2192d

---
+++ test/l2/ts_misc.rb
@@ -0,0 +1,16 @@
+# $Id$
+
+$:.unshift File.join(File.dirname(__FILE__), "../..", "lib")
+
+require 'test/unit'
+require 'racket'
+
+class TestL2Misc <  Test::Unit::TestCase
+  def test_convert
+    mac = "de:ad:ba:dc:af:e0"
+    long = Racket::L2::Misc.mac2long(mac)
+    assert_equal(mac, Racket::L2::Misc.long2mac(long))
+    assert_equal(long, Racket::L2::Misc.mac2long(mac))
+  end
+end
+# vim: set ts=2 et sw=2:
