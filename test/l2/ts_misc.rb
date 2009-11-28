2aafa48da8e6288bac0982dc42ef4d095134e669
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-11-28 21:38:47 +0000

    Test that proper number of : exist
    git-svn-id: http://spoofed.org/racket/svn/trunk@114 64fbf49a-8b99-41c6-b593-eb2128c2192d

--- test/l2/ts_misc.rb
+++ test/l2/ts_misc.rb
@@ -7,9 +7,10 @@ require 'racket'
 
 class TestL2Misc <  Test::Unit::TestCase
   def test_convert
-    (0..512).each {
-      len = rand(512)
+    (0..rand(1024)).each {
+      len = rand(512)+1
       mac = Racket::L2::Misc.randommac(len)
+      assert_equal(mac.length, (len*2) + (len-1))
       long = Racket::L2::Misc.mac2long(mac)
       assert_equal(mac, Racket::L2::Misc.long2mac(long, len))
       assert_equal(long, Racket::L2::Misc.mac2long(mac))
