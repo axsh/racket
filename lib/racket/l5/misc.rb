d3ee8be27786e1a02526cd1de73b3bf9c141006a
jhart <jhart@64fbf49a-8b99-41c6-b593-eb2128c2192d>
2009-12-18 05:19:57 +0000

    no need for ipaddr
    git-svn-id: http://spoofed.org/racket/svn/trunk@166 64fbf49a-8b99-41c6-b593-eb2128c2192d

--- lib/racket/l5/misc.rb
+++ lib/racket/l5/misc.rb
@@ -25,7 +25,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-require 'ipaddr'
 module Racket
 module L3
   # Miscelaneous L3 helper methods
